a475148057fdd389bfc4eda74c8dfdb843e6a8ac
Anand Narayana Perumal <anand@Anands-MacBook-Pro-7917.fritz.box>
2026-02-23 23:36:28 +0100

    Week1 Day1: Completed profile assignment with clean location variable

---
+++ week1/day1.py
@@ -0,0 +1,25 @@
+name = "Anand"
+age = 30
+height = 5.8
+is_engineer = True
+city = "Mannheim"
+country = "Germany"
+current_job = "QA Engineer"
+target_job = "AI Engineer"
+years_of_experience = 10
+expected_salary = 90000
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_engineer))
+
+print(f"My name is {name} and I am {age} years of old and I am {height} feet tall")
+print(f"Am I an Engineeer ? {is_engineer}")
+
+print(f"I am {name} from {country}.")
+print(f"Currently working as {current_job}.")
+print(f"I want to become {target_job}.")
+print(f"I have {years_of_experience} years of experience.")
+print(f"My expected salary is {expected_salary} Euros.")
+
---
+++ week1/day1_assignment.py
@@ -0,0 +1,19 @@
+name = "Anand"
+age = 35
+city = "Mannheim"
+country = "Germany"
+profession = "QA Engineer"
+target_role = "AI Engineer"
+skills = ["QA", "Automation", "Selenium"]
+is_learning_ai = True
+location = f"{city}, {country}"
+
+print(f"-------------- PROFILE ---------------")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Location: {location}")
+print(f"Current Role: {profession}")
+print(f"Target Role: {target_role}")
+print(f"Skills: {', '.join(skills)}")
+print(f"Learning AI: {is_learning_ai}")
+print(f"--------------------------------------")
